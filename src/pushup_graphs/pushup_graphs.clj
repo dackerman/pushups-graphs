dfcf9f38f9327aeeb5d671d1c7e96eb6078181b4
David Ackerman <david.w.ackerman@gmail.com>
2023-04-18 13:31:04 -0700

    Generated project

---
+++ src/pushup_graphs/pushup_graphs.clj
@@ -0,0 +1,12 @@
+(ns pushup-graphs.pushup-graphs
+  "FIXME: my new org.corfield.new/scratch project.")
+
+(defn exec
+  "Invoke me with clojure -X pushup-graphs.pushup-graphs/exec"
+  [opts]
+  (println "exec with" opts))
+
+(defn -main
+  "Invoke me with clojure -M -m pushup-graphs.pushup-graphs"
+  [& args]
+  (println "-main with" args))
